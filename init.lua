1bca5721e416dc34505ec2e67b0cee9b731e3f1c
suchdevdarshan7 <suchdev7@gmail.com>
2025-06-11 00:41:15 +0530

    fix: Added a new line of code for testing github

---
+++ init.lua
@@ -0,0 +1,160 @@
+-- Bootstrap packer.nvim if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- Plugin manager setup
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'ThePrimeagen/harpoon'
+  use 'folke/zen-mode.nvim'
+  use 'tpope/vim-fugitive'
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'mattn/emmet-vim'
+  use 'tpope/vim-surround'
+  use 'windwp/nvim-autopairs'
+
+  -- .NET Support
+  use 'nvim-neotest/nvim-nio'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  use 'jay-babu/mason-null-ls.nvim'
+  use 'nvimtools/none-ls.nvim'
+  use 'mfussenegger/nvim-dap'
+  use 'rcarriga/nvim-dap-ui'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+vim.cmd [[highlight Normal guibg=NONE]]
+
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.wildignore:append({ "C:/ProgramData/Sophos/**" })
+
+-- Key mappings
+vim.api.nvim_set_keymap('n', 'ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'tf', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'zm', ':ZenMode<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'tb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('x', '<leader>{', [[<Esc><i{<Esc>>a}<Esc>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<leader>(', [[<Esc><i(<Esc>>a)<Esc>]], { noremap = true, silent = true })
+
+vim.g.user_emmet_leader_key = '<C-y>,'
+vim.g.user_emmet_mode = 'jsx'
+
+-- LSP settings
+local lspconfig = require('lspconfig')
+lspconfig.ts_ls.setup {}
+lspconfig.pyright.setup {}
+lspconfig.rust_analyzer.setup {}
+
+-- Autopairs
+require('nvim-autopairs').setup({
+  disable_filetype = { "TelescopePrompt", "vim" },
+})
+
+-- Autocompletion setup
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+})
+
+-- Harpoon setup
+require('harpoon').setup()
+
+-- Zen Mode setup
+require('zen-mode').setup()
+
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "omnisharp" }
+})
+
+-- OmniSharp LSP setup
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').omnisharp.setup({
+  capabilities = capabilities,
+  cmd = { "omnisharp" },
+  enable_roslyn_analyzers = true,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+})
+
+-- Null-ls for formatting
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.csharpier
+  },
+})
+require("mason-null-ls").setup({
+  ensure_installed = { "csharpier" },
+  automatic_installation = true,
+})
+
+-- DAP for .NET Core
+local dap = require("dap")
+dap.adapters.coreclr = {
+  type = 'executable',
+  command = 'C:/tools/netcoredbg/netcoredbg.exe', -- Change this to your debugger path
+  args = {'--interpreter=vscode'}
+}
+dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "Launch - netcoredbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to DLL > ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end,
+  },
+}
+
+-- DAP UI
+require("dapui").setup()
+vim.keymap.set("n", "<F5>", require'dap'.continue)
+vim.keymap.set("n", "<F10>", require'dap'.step_over)
+vim.keymap.set("n", "<F11>", require'dap'.step_into)
+vim.keymap.set("n", "<F12>", require'dap'.step_out)
+vim.keymap.set("n", "<leader>du", require'dapui'.toggle)
+
+
